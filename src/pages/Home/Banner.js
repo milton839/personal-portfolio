91237a5e019fb66ea85c9fecf6ea329335d71f20
Milton839 <miltonbiswas839@gmail.com>
2022-05-24 23:24:48 +0600

    navpart responsive for mobile and desktop

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className="bg-red-900 py-40">
+        <div className="bg-indigo-800 py-40 border-t-2 border-gray-500">
             sdfsdfsdfs
         </div>
     );
--- src/pages/Shared/Navpart.js
+++ src/pages/Shared/Navpart.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiFillCloseCircle, AiOutlineMenu } from 'react-icons/ai';
 
 const Navpart = () => {
+    const [open, setOpen] = useState(false)
     const navcolor = {
         backgroundColor:'#152136'
     }
@@ -12,13 +14,18 @@ const Navpart = () => {
         {name: 'CONTACT', link:"/contact"},
     ]
     return (
-        <div className="py-10" style={navcolor}>
+        <div className="py-10 bg-indigo-800" >
             <div className="container mx-auto text-white md:flex justify-between font-bold text-4xl cursor-pointer pl-4">
                 <h2 className="md:pb-0 pb-4">MILTON</h2>
-                <ul className="md:flex text-white items-center absolute md:static md:z-auto z-[-1] w-full md:w-auto">
+                <div onClick={() => setOpen(!open)} className="text-4xl absolute right-8 top-11 cursor-pointer md:hidden border-2 border-white rounded">
+                    {
+                        open ? <AiFillCloseCircle /> : <AiOutlineMenu />
+                    }
+                </div>
+                <ul className={`md:flex md:text-white text-indigo-500 md:bg-indigo-800 bg-white md:items-center absolute md:static  md:z-auto left-0 w-full md:w-auto md:mt-0 mt-4 md:pt-0 pt-4 md:pb-0 pb-4 transition-all duration-500 ease-in ${open ? "left-0 opacity-100": "left-[-490px] md:opacity-100 opacity-0"} md:border-0`}>
                     {
                         navLinks.map((link) => (
-                            <li key={link.link} className="md:pl-6 text-xl duration-700 hover:text-green-700 md:pb-0 pb-4">
+                            <li key={link.link} className="md:pl-6 pl-4 text-xl duration-700 hover:text-green-700 md:pb-0 pb-4">
                                 <a href={link.link}>{link.name}</a>
                             </li>
                         ))
